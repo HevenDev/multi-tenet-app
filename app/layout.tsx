0f6a0477703e5840a6b970ab5c792b26d2e62f58
Chandra Prakash <hevendevofficial@gmail.com>
2025-08-03 13:08:23 +0000

    Organisation updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs"
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,14 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+
       >
         <ClerkProvider>
-          <div className="min-h-screen min-w-screen flex justify-center items-center">
-            <SignedOut>
-              <SignIn />
-            </SignedOut>
-          </div>
+          <SignedOut>
+            <div className="min-h-screen min-w-screen flex justify-center items-center">
+              <SignIn routing="hash" />
+            </div>
+          </SignedOut>
           <SignedIn>
             {children}
           </SignedIn>
